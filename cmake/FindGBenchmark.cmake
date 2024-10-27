fa402a4669a7902a47877a91f00f327375bfd445
Vadim Chaykin <snusmumrike2@gmail.com>
2024-10-27 18:08:59 +0300

    formatted

--- cmake/FindGBenchmark.cmake
+++ cmake/FindGBenchmark.cmake
@@ -1,9 +1,9 @@
 include(FetchContent)
 
 FetchContent_Declare(
-  google_benchmark
-  GIT_REPOSITORY https://github.com/google/benchmark.git
-  GIT_TAG        v1.9.0
+        google_benchmark
+        GIT_REPOSITORY https://github.com/google/benchmark.git
+        GIT_TAG v1.9.0
 )
 
 set(BENCHMARK_ENABLE_GTEST_TESTS OFF)
