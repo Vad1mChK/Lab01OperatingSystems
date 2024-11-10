c068ccd72ca160f410a7ef1b3553e30d8162ebd8
Vadim Chaykin <snusmumrike2@gmail.com>
2024-11-10 18:10:57 +0300

    Added macro to check for unistd

---
+++ source/monolith/shell/util/unistd_check.h
@@ -0,0 +1,14 @@
+#ifndef UNISTD_CHECK_H
+#define UNISTD_CHECK_H
+
+#ifdef __has_include
+  #if __has_include(<unistd.h>)
+    #define UNISTD_AVAILABLE 1
+  #else
+    #define UNISTD_AVAILABLE 0
+  #endif
+#else
+  #define UNISTD_AVAILABLE 0
+#endif // has_include
+
+#endif // UNISTD_CHECK_H
