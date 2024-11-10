ce1449fc8783b9fd3ae65ab08d3f8dad5cbdd2a2
Vadim Chaykin <snusmumrike2@gmail.com>
2024-11-10 19:56:23 +0300

    Extracted run logic to util file

---
+++ source/monolith/shell/util/run_functions.hpp
@@ -0,0 +1,12 @@
+//
+// Created by vad1mchk on 2024/11/10.
+//
+
+#ifndef RUN_FUNCTIONS_H
+#define RUN_FUNCTIONS_H
+#include <vector>
+#include <string>
+
+int RunProgram(std::vector<std::string>& args);
+
+#endif  // RUN_FUNCTIONS_H
