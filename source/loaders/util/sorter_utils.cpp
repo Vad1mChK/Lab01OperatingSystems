66ffb79d0fcc1c3714dab2e8e5f33289cb72cbf6
Vadim Chaykin <snusmumrike2@gmail.com>
2024-11-10 21:58:30 +0300

    Extracted constants and random for loaders

---
+++ source/loaders/util/sorter_utils.cpp
@@ -0,0 +1,9 @@
+#include "sorter_utils.hpp"
+#include <random>
+
+uint32_t RandomUint32() {
+  static std::random_device random_device;
+  static std::mt19937 engine(random_device());
+  static std::uniform_int_distribution<uint32_t> dist(0, std::numeric_limits<uint32_t>::max());
+  return dist(engine);
+}
---
+++ source/loaders/util/sorter_utils.hpp
@@ -0,0 +1,14 @@
+//
+// Created by vad1mchk on 2024/11/10.
+//
+
+#ifndef MONOLITH_SORTER_UTILS_HPP
+#define MONOLITH_SORTER_UTILS_HPP
+#include <cstddef>
+#include <cstdint>
+
+static const size_t BytesInMb = 1024 * 1024;
+
+uint32_t RandomUint32();
+
+#endif  // MONOLITH_SORTER_UTILS_HPP
